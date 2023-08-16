5d2448b628a74804bea227c84f48db184c3de676
jody ferniawan <jodyferniawan222@gmail.com>
2023-08-16 21:36:11 +0700

    upload project React Js

---
+++ src/pages/SyaratKetenPage.jsx
@@ -0,0 +1,50 @@
+import { Container, Row, Col } from "react-bootstrap"
+import FaqComponent from "../components/FaqComponent"
+
+const SyaratKetenPage = () => {
+  return (
+    <div className="syarat-ketentuan-page">
+      <div className="syarat-ketentuan min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="fw-bold text-center mb-2 animate__animated animate__fadeInUp animate__delay-1s">Syarat & Kententuan</h1>
+              <p className="text-center animate__animated animate__fadeInUp animate__delay-1s">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </Col>
+          </Row>
+          <Row className="pt-5">
+            <Col>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero aut dignissimos, corporis consequatur quam sunt vitae repellat quasi corrupti, minima voluptates amet, doloremque dicta laborum quo error reprehenderit. Eaque minus cupiditate, at odit obcaecati quaerat doloremque unde recusandae doloribus, ex voluptas sit praesentium deleniti omnis magnam quis laboriosam reprehenderit repellendus, culpa quasi quod dignissimos porro odio. Reiciendis optio architecto nam!</p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">1. Lorem</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptates aliquid unde nisi atque iusto quis odio ratione? Aliquid nostrum, dicta recusandae tempore, sapiente doloribus hic reprehenderit blanditiis amet aspernatur nam veritatis ex aliquam ratione asperiores unde facere distinctio nihil?</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio molestias vero blanditiis ipsam provident facere quaerat nesciunt architecto sapiente voluptate praesentium repellat, placeat recusandae dolores asperiores explicabo illum fugiat quod ex optio pariatur rerum facilis voluptates beatae. Commodi corporis reiciendis aspernatur illum, eius modi voluptates magni ut aliquid vero esse debitis facere voluptatum ab doloribus possimus molestiae, est quaerat. Modi?</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos repellendus, voluptates provident magnam molestiae laudantium nostrum aliquid exercitationem similique esse dolorum veritatis suscipit alias consequatur perferendis nihil fugiat atque maxime enim sed ex mollitia et error? Ad quibusdam cupiditate temporibus. Culpa exercitationem, esse mollitia ut suscipit, consequuntur facere expedita magni possimus ab aut praesentium ipsa, architecto minima modi est beatae totam dicta laudantium. Nisi, beatae dolore voluptatibus explicabo architecto possimus rerum odit repellat deserunt ut, itaque porro obcaecati. Totam, repellat!</p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">2. Lorem</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum repellat sit accusamus totam, perferendis soluta quam necessitatibus aliquid placeat non officia illo nostrum voluptas, repudiandae nulla cum quo nobis minus in? Consequuntur non nostrum deleniti omnis modi ex ipsum velit, necessitatibus delectus aliquid sed, ab assumenda eveniet. Praesentium veniam voluptatum magnam at error illum consectetur tempora quidem, aperiam optio architecto.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae voluptate consequuntur eaque nostrum dolorum dolor cupiditate natus reprehenderit voluptatibus ullam vero consectetur, voluptatum, repellendus culpa a cum facere repudiandae suscipit. Ipsum at, voluptate eveniet inventore ut velit nulla, minima nam illo qui, repellendus veritatis voluptas architecto! Sint repellat ab quas amet commodi, assumenda quis odit est sit explicabo praesentium molestiae qui pariatur. Alias nulla ipsum iste molestiae molestias amet pariatur, nostrum voluptas officiis eveniet velit nam, laborum aliquid fugiat quo.</p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">3. Lorem</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sint tenetur accusamus delectus deleniti qui dolor ipsa vero voluptatibus laborum nesciunt, earum amet unde aspernatur, vel, maxime tempore commodi odit. Autem vero fugiat ducimus, praesentium non nesciunt? Adipisci, voluptatibus placeat.</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi illum accusamus, quibusdam delectus exercitationem odit quis nihil error architecto corrupti possimus totam esse sed non natus qui, libero provident officia, impedit quasi quod facere? Totam, quam nobis illo officiis nihil distinctio commodi consectetur corrupti accusantium incidunt atque architecto omnis. Quo laudantium magnam doloremque, aperiam voluptatem non nemo. Totam, harum ea laborum quos iusto possimus nam dolores! Illum autem, exercitationem voluptate, nulla architecto alias labore amet, est consequatur impedit libero recusandae.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim ea animi repellat perspiciatis quas omnis necessitatibus ad delectus officiis sunt ipsam accusamus repellendus laudantium culpa eius, quibusdam aperiam labore nostrum nisi natus inventore veniam quaerat amet? Quam voluptatem aperiam nam tenetur autem velit! Doloribus magni et quaerat assumenda unde, alias magnam, illum odit perspiciatis libero iste, totam nisi labore officia.</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <FaqComponent/>
+    </div>
+  )
+}
+
+export default SyaratKetenPage
